49cca960de3ac94bb93efdb9cf67e7837e00ea5f
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-11-26 18:14:21 +0700

    create network model

---
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/model/LoanRequest.kt
@@ -0,0 +1,10 @@
+package com.focusstart.android.finalproject.loanmoneyonline.data.model
+
+data class LoanRequest(
+    val amount: Int?,
+    val firstName: String,
+    val lastName: String,
+    val percent: Double?,
+    val period: Int?,
+    val phoneNumber: String
+)
