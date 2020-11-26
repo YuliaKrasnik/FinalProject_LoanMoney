69d320cfd58ec73a6a7bfe48a15b1f829fc509a3
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-11-27 01:35:52 +0700

    created a dynamic replacement start destination

--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/ui/MainActivity.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/ui/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.focusstart.android.finalproject.loanmoneyonline.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import com.focusstart.android.finalproject.loanmoneyonline.Constants.APP_PREFERENCES
+import com.focusstart.android.finalproject.loanmoneyonline.Constants.APP_PREFERENCES_TOKEN
 import com.focusstart.android.finalproject.loanmoneyonline.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        setStartFragment()
     }
+
+    private fun setStartFragment() {
+        if (isSignedIn()) {
+            val navigationController = Navigation.findNavController(this, R.id.nav_host_fragment)
+            val graph = navigationController.graph
+            graph.startDestination = R.id.listOfLoansFragment
+            navigationController.graph = graph
+        }
+    }
+
+    private fun isSignedIn(): Boolean {
+        if (getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE).contains(
+                APP_PREFERENCES_TOKEN
+            )
+        ) {
+            val token = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE).getString(
+                APP_PREFERENCES_TOKEN,
+                ""
+            )
+            return token != "" && token != null
+        }
+        return false
+    }
+
 }
