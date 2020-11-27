826e73d8febcb1e71f7654ae31ead851d1684510
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-11-27 19:31:44 +0700

    add token in which one loan

--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/datasource/LoanDataSource.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/datasource/LoanDataSource.kt
@@ -13,7 +13,8 @@ interface LoanDataSource {
 }
 class LoanDataSourceImpl : LoanDataSource {
     private val apiService by lazy { RetrofitBuilder.buildService(ILoanApi::class.java) }
-    private val token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5biIsImV4cCI6MTYwOTA1NjIwN30.t1xr1AbUFVnQgEU8CFlofh-i2IKqGYq6cyYVTOAMyfpJpgAPc7QweL6rHFkBlSZeraFotQqqhPtiznKKp2fWMA" //
+  //  private val token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5biIsImV4cCI6MTYwOTA1NjIwN30.t1xr1AbUFVnQgEU8CFlofh-i2IKqGYq6cyYVTOAMyfpJpgAPc7QweL6rHFkBlSZeraFotQqqhPtiznKKp2fWMA" //
+    private val token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2MDkwNjkxNjN9.tWn3YAlka6hHiG2mz7x6G70u-h9USLfcR1qnLCANSvwKk3ri8v-n1qCCMuaJXHoyra2w4UPXLwVfYpsLicctmg" //
 
     override fun getLoansList(): Single<Response<List<Loan>>> {
         return apiService.getLoansList(token)    //TODO достать значение из sharedPref в data слое
