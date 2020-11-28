d5d2392949f2088de5617619e92a926437f457df
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-11-28 16:41:07 +0700

    created splash activity

---
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/ui/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.focusstart.android.finalproject.loanmoneyonline.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
