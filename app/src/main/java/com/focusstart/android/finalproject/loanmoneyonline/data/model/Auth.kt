cb1ee9141b720ed77db7423dd6a1e71234a0731a
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-11-29 17:40:47 +0700

    removed null type in model

--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/model/Auth.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/model/Auth.kt
@@ -1,6 +1,6 @@
 package com.focusstart.android.finalproject.loanmoneyonline.data.model
 
 data class Auth(
-    val name:String?,
-    val password: String?
+    val name:String,
+    val password: String
 )
--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/model/LoanRequest.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/data/model/LoanRequest.kt
@@ -1,10 +1,10 @@
 package com.focusstart.android.finalproject.loanmoneyonline.data.model
 
 data class LoanRequest(
-    val amount: Int?,
+    val amount: Int,
     val firstName: String,
     val lastName: String,
-    val percent: Double?,
-    val period: Int?,
+    val percent: Double,
+    val period: Int,
     val phoneNumber: String
 )
