c7f67f53bb8836890a3405b6ccd9b7e91ee8bc50
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-12-02 15:09:03 +0700

    added methods for transform status, color, date loans

--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/features/loans/features/listOsLoans/ui/ListAdapter.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/features/loans/features/listOsLoans/ui/ListAdapter.kt
@@ -1,12 +1,17 @@
 package com.focusstart.android.finalproject.loanmoneyonline.features.loans.features.listOsLoans.ui
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.focusstart.android.finalproject.loanmoneyonline.R
 import com.focusstart.android.finalproject.loanmoneyonline.features.loans.data.model.Loan
 
-class ListAdapter(private val clickFunction: (Loan) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
+class ListAdapter(
+        private val determineColorState: (String, Resources) -> Int,
+        private val transformNameState: (String, Resources) -> String,
+        private val transformDate: (String, Resources) -> String,
+        private val clickFunction: (Loan) -> Unit) : RecyclerView.Adapter<ItemViewHolder>() {
 
     private val itemsList = mutableListOf<Loan>()
 
@@ -18,7 +23,7 @@ class ListAdapter(private val clickFunction: (Loan) -> Unit) : RecyclerView.Adap
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
-        return ItemViewHolder(view, clickFunction)
+        return ItemViewHolder(view, determineColorState, transformNameState, transformDate, clickFunction)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
