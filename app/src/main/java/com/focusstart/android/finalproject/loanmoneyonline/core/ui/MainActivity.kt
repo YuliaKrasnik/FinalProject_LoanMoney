4c35a60884ba888ee16033e7c631ad0ab69ca3be
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-12-12 18:16:51 +0700

    removed unused imports

--- app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/core/ui/MainActivity.kt
+++ app/src/main/java/com/focusstart/android/finalproject/loanmoneyonline/core/ui/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.focusstart.android.finalproject.loanmoneyonline.core.ui
 
 import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.focusstart.android.finalproject.loanmoneyonline.App
 import com.focusstart.android.finalproject.loanmoneyonline.R
-import com.focusstart.android.finalproject.loanmoneyonline.core.data.provider.ISettingsProvider
-import com.focusstart.android.finalproject.loanmoneyonline.core.data.provider.SettingsProviderImpl
 import com.focusstart.android.finalproject.loanmoneyonline.core.di.main.module.MainPresenterModule
 import com.focusstart.android.finalproject.loanmoneyonline.core.presentation.IMainPresenter
 import com.focusstart.android.finalproject.loanmoneyonline.core.presentation.IMainView
-import com.focusstart.android.finalproject.loanmoneyonline.utils.Constants
-import java.util.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), IMainView {
